496475873b0b0f0cdbdf4973f4e2dd5b32fc5e9e
jaksylwia <ceneemail@gmail.com>
2021-03-15 13:35:21 +0100

    add create visit view and add create visit button to visit list

--- src/Controller/VisitController.php
+++ src/Controller/VisitController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Animal;
+use App\Entity\Visit;
+use App\Form\VisitType;
 use App\Repository\VisitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,6 +26,44 @@ class VisitController extends AbstractController
             'visit/index.html.twig',
             [
                 'visits' => $visits,
+                'animal' => $animal
+            ]
+        );
+    }
+
+    /**
+     * @Route("/animal/{id}/visit/create", name="create_visit")
+     */
+    public function create(Animal $animal, Request $request): Response
+    {
+        $visit = new Visit();
+        $form = $this->createForm(VisitType::class, $visit);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $visit->setAnimal($animal);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($visit);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                'Dodano nową wizytę!'
+            );
+
+            return $this->redirectToRoute(
+                'visit',
+                [
+                    'id' => $animal->getId()
+                ]
+            );
+        }
+
+        return $this->render(
+            'visit/create.html.twig',
+            [
+                'form' => $form->createView(),
                 'animal' => $animal
             ]
         );
--- src/Form/VisitType.php
+++ src/Form/VisitType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Visit;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,7 @@ class VisitType extends AbstractType
         $builder
             ->add('date')
             ->add('description')
-            ->add('save')
+            ->add('save', SubmitType::class)
         ;
     }
 
