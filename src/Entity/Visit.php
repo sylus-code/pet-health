f918682d055545ea1e74f0026950c98248168462
jaksylwia <ceneemail@gmail.com>
2021-03-18 15:51:28 +0100

    add symptom and symptom list view

--- src/Entity/Visit.php
+++ src/Entity/Visit.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\VisitRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -32,6 +34,16 @@ class Visit
      * @ORM\JoinColumn(nullable=false)
      */
     private $animal;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Symptom::class, mappedBy="visits")
+     */
+    private $symptoms;
+
+    public function __construct()
+    {
+        $this->symptoms = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -70,6 +82,33 @@ class Visit
     public function setAnimal(?Animal $animal): self
     {
         $this->animal = $animal;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Symptom[]
+     */
+    public function getSymptoms(): Collection
+    {
+        return $this->symptoms;
+    }
+
+    public function addSymptom(Symptom $symptom): self
+    {
+        if (!$this->symptoms->contains($symptom)) {
+            $this->symptoms[] = $symptom;
+            $symptom->addVisit($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSymptom(Symptom $symptom): self
+    {
+        if ($this->symptoms->removeElement($symptom)) {
+            $symptom->removeVisit($this);
+        }
 
         return $this;
     }
