6e54ec654c3ecca33ae1d36c51fddabb4400c980
jaksylwia <ceneemail@gmail.com>
2021-04-06 21:23:15 +0200

    fix login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,7 +15,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
          if ($this->getUser()) {
-             return $this->redirectToRoute('index');
+             return $this->redirectToRoute('animal_index');
          }
 
         // get the login error if there is one
